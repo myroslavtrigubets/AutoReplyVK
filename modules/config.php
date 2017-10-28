5076e1271fcb113d296ae36b51b5421d7f8d070d
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-10-28 11:58:41 +0300

    Update config.php

--- modules/config.php
+++ modules/config.php
@@ -1,8 +1,8 @@
 <?php
 
 $config = array(
-    'token' => 'b9c368406e4357b98d7251b0f30faabd7d7c286e69e3bef1104520e6b9bfd4fbd7859e67ef8b7e8771b0d',
-    'yourid' => '372648571');
+    'token' => '%VKTOKEN%',
+    'yourid' => '%ID%');
 
 $dictionary = array(
     'Привіт' => 'Привіт',
